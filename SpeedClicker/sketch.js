33d24394a153ee9c40321744b6cbc944641a3616
whobat <whobat@whobat.dk>
2024-11-11 13:17:27 +0100

    Added SpeedClicker to the collection

---
+++ SpeedClicker/sketch.js
@@ -0,0 +1,111 @@
+let gameState = 'INTRO';
+let score = 0;
+let timeLeft = 30;
+let lastTime = 0;
+let clickSound;
+let circleSize;
+
+function preload() {
+  clickSound = loadSound('res/snd/click.mp3'); // Add a click sound file to your project
+  gameOverSound = loadSound('res/snd/game-over.mp3');
+  imgBreefer = loadImage("res/img/breefers/Breefer_03.png");
+  imgBG = loadImage("res/img/Background.jpg")
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  circleSize = min(width, height) * 0.2;
+}
+
+function draw() {
+  background(50);
+  imageMode(CORNER);
+  image(imgBG, 0, 0, width, height, 0, 0, imgBG.height, COVER);
+  
+  switch(gameState) {
+    case 'INTRO':
+      drawIntro();
+      break;
+    case 'GAMEPLAY':
+      drawGameplay();
+      break;
+    case 'GAMEOVER':
+      drawGameover();
+      break;
+  }
+}
+
+function drawIntro() {
+  textSize(min(width, height) * 0.08);
+  fill("#eb6608");
+  text('SPEED CLICKER', width/2, height/3);
+  
+  textSize(min(width, height) * 0.03);
+  text('Klik på maskotten så mange gange du kan!\ntryk for at starte', width/2, height/2);
+}
+
+function drawGameover() {
+    textSize(min(width, height) * 0.08);
+    fill("#eb6608");
+    text('GAME OVER', width/2, height/3);
+    
+    textSize(min(width, height) * 0.05);
+    text('Score: ' + score, width/2, height/2);
+    noLoop();
+    gameOverSound.play();
+    // Send score to parent React component
+    window.parent.postMessage({ score }, "*");
+  }
+  
+
+function drawGameplay() {
+  if (millis() - lastTime >= 1000) {
+    timeLeft--;
+    lastTime = millis();
+    if (timeLeft <= 0) {
+      gameState = 'GAMEOVER';
+    }
+  }
+  
+  // Draw timer at bottom
+  textSize(min(width, height) * 0.05);
+  fill("#eb6608");
+  text(timeLeft, width/2, height * 0.9);
+  
+  // Draw score
+  text(score, width/2, height/6);
+  
+  // Draw Breefer
+  imageMode(CENTER);
+  image(imgBreefer, width/2, height/2, width/3, height/3)
+  
+}
+
+
+function mousePressed() {
+  if (gameState === 'INTRO') {
+    gameState = 'GAMEPLAY';
+    score = 0;
+    timeLeft = 30;
+    lastTime = millis();
+  } else if (gameState === 'GAMEPLAY') {
+    let d = dist(mouseX, mouseY, width/2, height/2);
+    if (d < circleSize/2) {
+      score++;
+      clickSound.play();
+    }
+  } else if (gameState === 'GAMEOVER') {
+    
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  circleSize = min(width, height) * 0.2;
+}
+
+function touchStarted() {
+  // Prevent default touch behavior on mobile
+  return false;
+} 
